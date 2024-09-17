99bfb04e08a1d11cffc68b13add8448754ce97d7
Alan Russell <ajrussellaudio@gmail.com>
2024-09-17 17:27:14 +0100

    Add Prisma schema

---
+++ script.ts
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // const user = await prisma.user.create({
+  //   data: {
+  //     name: 'Alan',
+  //     email: 'alan@prisma.io',
+  //     type: 'TEACHER',
+  //   },
+  // });
+  // console.log(user);
+  const users = await prisma.user.findMany();
+  console.log(users);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
