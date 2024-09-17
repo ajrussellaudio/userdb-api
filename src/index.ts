81425829ec0fe8dd90a3bef5cbbcdbdf821f4b92
Alan Russell <ajrussellaudio@gmail.com>
2024-09-17 15:19:09 +0100

    Prettify the boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (_req, res) => {
   res.send('Hello World!');
 });
 
